4e8a94df01a1c1c9622314c966544280565a916e
Sigit Wahyudi <wahyudi.sigit@gmail>
2022-11-09 15:56:51 +0800

    first set client mkt

---
+++ routes/web.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use \RouterOS\Client;
+use \RouterOS\Query;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+
+// Initiate client with config object
+$client = new Client([
+    'host' => '172.16.20.1',
+    'user' => 'adm',
+    'pass' => 'NOCsisfokom08',
+    'port' => 19745,
+]);
+
+// Create "where" Query object for RouterOS
+$query =
+    // (new Query('/ip/hotspot/ip-binding/print'));
+    (new Query('/ip/hotspot/user/print'));
+
+// Send query and read response from RouterOS
+$response = $client->query($query)->read();
+$response = json_encode($response);
+
+echo $response;
+});
